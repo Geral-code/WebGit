ef03b752eb95594da0f4985c7775c763ad0adcb5
Geraldine <geraldine.orellana@gmail.com>
2022-05-25 19:47:23 -0400

    container con funcionalidad en js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log('hola mundo');
+const container = document.querySelector('.container');
+
+//crear un parrafo
+const parrafo = document.createElement('p');
+
+//texto parrafo 
+const textParrafo = document.createTextNode('mi parrafo');
+
+//añadiendo el texto al parrafo
+parrafo.appendChild(textParrafo);
+
+//renderizando en  el Dom
+container.append(parrafo);
